7f392453f26f0d97dda9dc1c2baac06a880a2f25
Bharat Singh <bksingh@g.cofc.edu>
2017-03-25 15:35:52 -0400

    My HW

---
+++ bharatSinghJS1/Exercises/exercise1.js
@@ -0,0 +1,9 @@
+// Exercises
+
+// 1. Write two small JavaScript statements and add comments above each describing the code.
+// Declaring a variable named 'x'.
+var x;
+
+// Assigning a numerical value of 7 to variable 'x'.
+x = 7;
+
