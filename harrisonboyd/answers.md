8e07e8e439091ae97e49fe074cd979f6c33e769a
Harrison Boyd <harrisonboyd630@gmail.com>
2017-03-31 09:57:01 -0400

    hw

---
+++ harrisonboyd/answers.md
@@ -0,0 +1,21 @@
+Why should you always link external JavaScript files at the end of the <body> tag?
+
+It helps with performance and loading of the page when the <script> is at the bottom.
+
+Why is using comments important?
+
+Answer: To keep track of what you have done and why you did it so when you go back later or if someone else looks it your code, you or them will know why something was done the way it was.
+
+List the 5 primitive types in JavaScript.
+
+Answer: Number, string, boolean, null, undefined
+
+Once a variable has been defined, can its value be changed?
+
+a. yes
+
+b. no
+
+c. it depends
+
+Answer: C. it depends. Var declared locally can not be changed outside of the function they are delcared in. Global functions can be changed however.
---
+++ harrisonboyd/javascript-exercises1.js
@@ -0,0 +1,59 @@
+//Problem 1
+
+var firstName = "harrison";
+
+//On line 3 I wrote a simple variable statement. I gave the variable a value of firstName and set it equal to a string of "harrison"
+
+
+console.log(firstName);
+
+//On line 8 I console.log the variable I declared on line 3 to the console. 
+
+//Problem 2
+
+var firstName = "Jack";
+var lastName = "Smith";
+
+function greet (first, last) {
+  return "Good morning, " + first + " " + last
+};
+
+greet(firstName, lastName);
+
+//Problem 3
+
+// a:
+var result = "The cat jumped over the moon";
+//string
+
+// b:
+var result = 55 - 27;
+//number
+
+// c:
+var result = 75 > 55;
+//boolean
+
+// d:
+var data;
+var result = data;
+//undefined
+
+// e:
+var result = -6544.432;
+//number
+
+//Problem 4
+
+// here's the first variable assignment you'll need
+// you'll need to to assign the other 2 numbers yourself
+var firstNumber = 50;
+var secondNumber = 100;
+var thirdNumber = 500
+
+// add them together using + and store them in the sum variable
+var sum = firstNumber + secondNumber + thirdNumber;
+
+// finally, print the sum
+console.log(sum);
+
